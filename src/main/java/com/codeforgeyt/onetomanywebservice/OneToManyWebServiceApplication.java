c6cb25af098c32daa5a1785dd5c60c458f20cf2b
codeforgeyt <codeforgeyt@gmail.com>
2021-01-03 11:20:07 +0100

    Add cors config, spring boot admin, example conifguration

--- src/main/java/com/codeforgeyt/onetomanywebservice/OneToManyWebServiceApplication.java
+++ src/main/java/com/codeforgeyt/onetomanywebservice/OneToManyWebServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OneToManyWebServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OneToManyWebServiceApplication.class, args);
 	}
---
+++ src/main/java/com/codeforgeyt/onetomanywebservice/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.codeforgeyt.onetomanywebservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${allowed.origin}")
+    private String allowedOrigin;
+
+    @Bean
+    public WebMvcConfigurer getCorsConfiguration(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigin)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
