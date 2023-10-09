5b855e8006da50edaaeb027d393402f63a4c4b02
pahwamonil <56338123+pahwamonil@users.noreply.github.com>
2023-10-09 14:09:09 +0530

    Update ItemController.java
    GetMapping we can apply only on method level and RequestMapping annotation we can apply on class level and as well as on method level

--- src/main/java/com/codeforgeyt/onetomanywebservice/controller/ItemController.java
+++ src/main/java/com/codeforgeyt/onetomanywebservice/controller/ItemController.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/items")
 public class ItemController {
 
     private final ItemService itemService;
@@ -22,32 +21,32 @@ public class ItemController {
         this.itemService = itemService;
     }
 
-    @PostMapping
+    @RequestMapping(value = "/postitems", method = RequestMethod.POST, produces = "application/json") 
     public ResponseEntity<ItemDto> addItem(@RequestBody final ItemDto itemDto){
         Item item = itemService.addItem(Item.from(itemDto));
         return new ResponseEntity<>(ItemDto.from(item), HttpStatus.OK);
     }
 
-    @GetMapping
+    @RequestMapping(value = "/getitems", method = RequestMethod.GET, produces = "application/json") 
     public ResponseEntity<List<ItemDto>> getItems(){
         List<Item> items = itemService.getItems();
         List<ItemDto> itemsDto = items.stream().map(ItemDto::from).collect(Collectors.toList());
         return new ResponseEntity<>(itemsDto, HttpStatus.OK);
     }
 
-    @GetMapping(value = "{id}")
+    @RequestMapping(value = "/getitems/{id}", method = RequestMethod.GET, produces = "application/json") 
     public ResponseEntity<ItemDto> getItem(@PathVariable final Long id){
         Item item = itemService.getItem(id);
         return new ResponseEntity<>(ItemDto.from(item), HttpStatus.OK);
     }
 
-    @DeleteMapping(value = "{id}")
+    @RequestMapping(value = "/deleteitems/{id}", method = RequestMethod.DELETE, produces = "application/json") 
     public ResponseEntity<ItemDto> deleteItem(@PathVariable final Long id){
         Item item = itemService.deleteItem(id);
         return new ResponseEntity<>(ItemDto.from(item), HttpStatus.OK);
     }
 
-    @PutMapping(value = "{id}")
+    @RequestMapping(value = "/updateitems/{id}", method = RequestMethod.PUT, produces = "application/json") 
     public ResponseEntity<ItemDto> editItem(@PathVariable final Long id,
                                             @RequestBody final ItemDto itemDto){
         Item editedItem = itemService.editItem(id, Item.from(itemDto));
